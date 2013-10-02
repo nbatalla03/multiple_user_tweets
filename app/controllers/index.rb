5a8c6c43730bdee97b7d46f98bf546036cd2a4c1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-02 09:18:26 -0500

    authenticated twitter users and twitter postings

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+# GET  =========================================================================
+
 get '/' do
   erb :index
 end
@@ -18,8 +20,27 @@ get '/auth' do
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
+  @twitter_username = @access_token.params[:screen_name]
+  twitter_token = @access_token.params[:oauth_token]
+  twitter_secret = @access_token.params[:oauth_token_secret]
+
   # at this point in the code is where you'll need to create your user account and store the access token
 
-  erb :index
-  
+  @user = User.find_by_username(@twitter_username)
+  # @user = User.create(username: "#{@twitter_username}", oauth_token: twitter_token, oauth_secret: twitter_secret)
+
+  if !@user
+    @user = User.create(username: "#{@twitter_username}", oauth_token: twitter_token, oauth_secret: twitter_secret)
+  end
+
+  erb :authorized
+end
+
+# POST =========================================================================
+
+post '/twitter_update' do
+  user = User.find_by_username(params[:twitter_username])
+  twitter_user = Twitter::Client.new( oauth_token: user.oauth_token, oauth_token_secret: user.oauth_secret)
+  twitter_user.update(params[:tweet])
+  redirect '/auth'
 end
